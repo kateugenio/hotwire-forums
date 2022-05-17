0deb18b7fc3428de5375d135ee603969d3307b77
Katherine Eugenio <keugenio@wrapbook.com>
2022-05-17 11:31:11 -0700

    Add categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   devise_for :users
   resources :discussions do
     resources :posts, only: [:create, :show, :edit, :update, :destroy], module: :discussions
