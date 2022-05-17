2d324f0a27afa607942d2d7e837bf0108af9944a
Katherine Eugenio <keugenio@wrapbook.com>
2022-05-17 13:00:47 -0700

    add category filtering

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :discussions do
     resources :posts, only: [:create, :show, :edit, :update, :destroy], module: :discussions
+
+    collection do
+      get 'categories/:id', to: 'categories/discussions#index', as: :category
+    end
   end
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
